7136e55ffbcb4fc6d5842f99f559a680e396a1dc
ssfwork <shishunfan+work@gmail.com>
2024-09-08 01:51:30 +1200

    try remove image config

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const isProd = process.env.NODE_ENV == "production"
 const nextConfig = {
   basePath: isProd ? "/ssfspace" : "",
   output: "export",
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
