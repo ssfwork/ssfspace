44ad798d6d3d7e7aa15a2187f6a1b55ee097e100
ssfwork <shishunfan+work@gmail.com>
2024-09-08 01:44:46 +1200

    remove unused code

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,9 @@
-"use client"
-import { useRouter } from "next/router"
 import Image from "next/image"
 import Link from "next/link"
 import Hero from "@/components/hero"
 import AboutImage from "@/public/images/me_tongariro_02.jpg"
 
 export default function About() {
-  const { basePath } = useRouter()
-
   return (
     <div className="grow md:flex space-y-8 md:space-y-0 md:space-x-8 pt-12 md:pt-16 pb-16 md:pb-20">
       {/* Middle area */}
