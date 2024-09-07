46fe2bfcc80b32b7c3db7e2a17bfdcbf5d388b4d
ssfwork <shishunfan+work@gmail.com>
2024-09-07 22:34:10 +1200

    update config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isExport = process.env.NEXT_EXPORT === "true"
+const isExport = false
 
 const nextConfig = {
   output: isExport ? "export" : undefined,
   trailingSlash: true,
   basePath: isExport ? "/ssfspace" : undefined,
-  experimental: {
-    esmExternals: true,
-  },
 }
 
 module.exports = nextConfig
